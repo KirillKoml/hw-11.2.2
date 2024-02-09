1438c3d07b85f4d0c0b20aae0a11d38fc8c0a86c
PS'Killer <komliev1999@mail.ru>
2024-02-09 20:56:34 +0500

    func

--- Task2/str_func.py
+++ Task2/str_func.py
@@ -1,2 +1,8 @@
+#функция выводит слова в верхнем регистре
 value = 'hello world'
 print(value.upper())
+
+#функция которая делает первую букву каждого слова в строке заглавной
+s = 'hello world'
+s = s.title()
+print(s)  # Вывод: 'Hello World'
